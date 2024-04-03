694ecd50b50a994caab5d5a440c534606bb64763
Thomas <giasinbenchafsi@gmail.com>
2024-04-03 23:53:00 +0300

    Added post player

--- br-web-app/app/api/players/route.js
+++ br-web-app/app/api/players/route.js
@@ -7,3 +7,28 @@ export async function GET() {
   const players =  await Player.find();
   return NextResponse.json({ players });
 }
+
+export async function POST(request) {
+  await dbConnect();
+  
+  const { name, kills, deaths, matchesPlayed } = await request.json();
+
+  // If no player is found, a new one is created with the provided stats.
+  const player = await Player.findOneAndUpdate(
+    { name: name }, // Find a player by this name
+    {
+      $inc: { // Increment the existing stats
+        kills: kills,
+        deaths: deaths,
+        matchesPlayed: matchesPlayed
+      }
+    },
+    {
+      new: true, // Return the updated document
+      upsert: true, // Create a new document if one doesn't exist
+      setDefaultsOnInsert: true // Apply model defaults if a new document is created
+    }
+  );
+
+  return NextResponse.json({ player });
+}
